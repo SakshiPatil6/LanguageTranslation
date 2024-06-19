32bc166d633850d3e776a10d0e3ed353f808c3b3
Sakshi Balaso Patil <sakshipatil3830@gmail.com>
2024-06-19 15:09:41 +0530

    Uploading

--- WebAppTranslation.py
+++ WebAppTranslation.py
@@ -1,14 +1,10 @@
-<<<<<<< HEAD
 import gradio as gr         #UI library
 from transformers import pipeline      #transformer pipeline
 
 translation_pipeline= pipeline('translation', model='Helsinki-NLP/opus-mt-en-es')
 result=translation_pipeline("My name is Sakshi")
-=======
-import gradio as gr         #UI library
-from transformers import pipeline      #transformer pipeline
-
-translation_pipeline= pipeline('translation', model='Helsinki-NLP/opus-mt-en-es')
-result=translation_pipeline("My name is Sakshi")
->>>>>>> 13abc0eefe707739571d301d935e89fc6ae41d87
 print(result[0]['translation_text'])
+
+def translate_transformer(from_text):
+    results = translation_pipeline(from_text)
+    return result[0]['translation_text']
